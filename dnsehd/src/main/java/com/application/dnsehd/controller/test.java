a14455e48ddfd26c5d5c7b1c869aa3732883c1f4
yohan <yohan@dansaem>
2024-01-10 19:26:16 +0900

    .

--- dnsehd/src/main/java/com/application/dnsehd/controller/test.java
+++ dnsehd/src/main/java/com/application/dnsehd/controller/test.java
@@ -1,4 +1,4 @@
-package com.application.dnsehd;
+package com.application.dnsehd.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
