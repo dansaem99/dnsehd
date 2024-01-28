bfc7e5727c657106f1b5492272983b5f8fc5ad58
dansaem <dansaem@dansaem>
2024-01-28 20:01:24 +0900

    .

--- dnsehd/src/main/java/com/application/dnsehd/config/SecurityConfiguration.java
+++ dnsehd/src/main/java/com/application/dnsehd/config/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package com.application.dnsehd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,15 @@ public class SecurityConfiguration{
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors().disable()			
+			.csrf().disable()			
+			.formLogin().disable()		
+			.headers().frameOptions().disable();
+ 
+		return http.build();
+	}
     
 }
